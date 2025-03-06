4a147a495ff5ebc406e8f38c5c6127ac53205999
stereolithography <jelewism@colostate.edu>
2025-03-05 18:20:40 -0700

    Another Commit

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -1,28 +1,49 @@
-﻿using UnityEngine;
+using UnityEngine;
 using CreatorKitCode;
+using UnityEngine.Audio;
 
 public class SpawnerSample : MonoBehaviour
+
 {
     public GameObject ObjectToSpawn;
-
     void Start()
     {
-        int angle = 15;
-        Vector3 spawnPosition = transform.position;
+        LootAngle myLootAngle = new LootAngle(45);
+
+        //every call will advance the angle!
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+        SpawnPotion(myLootAngle.NextAngle());
+
+    }
+    void SpawnPotion(int angle)
+    {
+        int radius = 5;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
 
+public class LootAngle
+
+{
+    int angle;
+    int step;
+
+    public LootAngle(int increment)
+    {
+        step = increment;
+        angle = 0;
+    }
+
+    public int NextAngle()
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle+ step);
+
+        return currentAngle;
+    }
+}
